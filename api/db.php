5c76c907c2aa5c92f7331d401d750e71aa77bdce
Roy Iacob <iacobroy@gmail.com>
2013-09-07 08:00:17 -0700

    fixes to db

--- api/db.php
+++ api/db.php
@@ -1,12 +1,15 @@
-<?php
-class DB 
-{
+<?php 
 
-    function __construct() 
-    {
+
+class DB {
+
+    
+
+    function __construct(){
         $host = "localhost"; 
         $user = "ubuntu"; 
         $r = mysql_connect($host, $user );
+
         if (!$r) 
         {
                 echo "Could not connect to server\n";
@@ -17,41 +20,31 @@ class DB
                 echo "Connection established\n"; 
                 $r2 = mysql_select_db("test");
 
-                if (!$r2) 
-                {
+                if (!$r2) {
                         echo "Cannot select database\n";
-                        trigger_error(mysql_error(), E_USER_ERROR); 
-                } 
-                else 
-                {
+                            trigger_error(mysql_error(), E_USER_ERROR); 
+                } else {
                         echo "Database selected\n";
                 }
 
+
                 $rs = mysql_query("SELECT * FROM users");
-                if (!$rs) 
-                {
-                    echo "Could not execute query: $query";
-                    trigger_error(mysql_error(), E_USER_ERROR); 
-                } else 
-                {
-                    echo "Query: query executed\n";
+                if (!$rs) {
+                        echo "Could not execute query: $query";
+                            trigger_error(mysql_error(), E_USER_ERROR); 
+                } else {
+                        echo "Query: query executed\n";
                 } 
 
-                while ($row = mysql_fetch_assoc($rs)) 
-                {
-                    echo $row['id'] . " " . $row['username'] . " " . $row['password'] . "\n";
+                while ($row = mysql_fetch_assoc($rs)) {
+                        echo $row['id'] . " " . $row['username'] . " " . $row['password'] . "\n";
                 }
 
                 mysql_close();
         }
-
-
-
     }
-
 }
-
-$db = new DB()
+$db = new DB();
 
 
 ?>
