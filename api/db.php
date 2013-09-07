007ec9e6bf66a89026d4ef98a40eee89cebe66e0
Roy <iacobroy@gmail.com>
2013-09-07 17:36:35 +0000

    stuff

--- api/db.php
+++ api/db.php
@@ -1,19 +1,16 @@
 <?php 
-
-
-class DB {
-
-    
-
+class DB{
     function __construct(){
         $host = "localhost"; 
         $user = "ubuntu"; 
-        $r = mysql_connect($host, $user);
-
-	echo "constructing";
+	//phpinfo();
+	//ini_set('display_errors',1);
+	//error_reporting(E_ALL);
+        $r = mysql_connect($host, $user);// or die('error '.mysqli_error($r));
+	//if (mysqli_connect_errno($r)) echo "failed" . mysqli_connect_error();	
         if (!$r) 
         {
-                echo "Could not connect to server\n";
+                echo "\nCould not connect to server\n";
                 trigger_error(mysql_error(), E_USER_ERROR);
         } 
         else 
@@ -26,13 +23,13 @@ class DB {
                 } 
                 
         }
-	echo "consturcted";
+	echo " constructed";
     }
 
     function insert_user($email,$password,$username) {
         $this->run_query("INSERT INTO users (email,password,username, karma,courses) VALUES ('$email','$password','$username',0,'')");
-
     }
+
 
 
     function run_query($query) {
@@ -44,7 +41,6 @@ class DB {
             return;
         } 
         return $rs;
-
     }
 
     function run_select() {
@@ -58,7 +54,7 @@ class DB {
 
         while ($row = mysql_fetch_assoc($rs)) 
         {
-                echo $row['id'] . " " . $row['username'] . " " . $row['password'];
+                echo $row['id'] . " email: " . $row['email'] . " pw: " . $row['password'] . '<br>';
         }
 
     }
@@ -66,9 +62,6 @@ class DB {
 	{
 		mysql_close();
 	}
-
-
 }
-$db = new DB();
 ?>
 
