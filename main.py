d0bc66b03fb479ac35fccf634cf928ac8df8936b
Michael Bock <mykel.bock@gmail.com>
2013-09-07 22:44:55 -0400

    switching stuff to gae

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi
+import re
+import os
+import jinja2
+import urllib2
+from xml.dom import minidom
+from string import letters
+import hashlib
+import hmac
+import logging
+import json
+from datetime import datetime, time
+from google.appengine.api import memcache
+from google.appengine.ext import db
+
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+def hash_str(s):
+	return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+	return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+
+def render_str(template, **params):
+	t = jinja_environment.get_template(template)
+	return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+	def render(self, template, **kw):
+		self.response.out.write(render_str(template, **kw))
+
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render('index.html')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
